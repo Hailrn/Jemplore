560c587751700a6838b7bdbdf1365bd1f1be13ec
Hailrn <xwildan07@gmail.com>
2025-11-25 17:13:50 +0700

    update: routes, Wildan

--- routes/web.php
+++ routes/web.php
@@ -6,34 +6,40 @@ Route::get('/', function () {
     return view('landing');
 });
 
-Route::get('/', function () {
-    return view('dasboardowner');
-});
-
-Route::get('/', function () {
-    return view('manageevents');
-});
-
-Route::get('/', function () {
-    return view('halkelolaprofil');
-});
-
-Route::get('/', function () {
-    return view('submission');
-});
-
-Route::get('/', function () {
-    return view('performance');
-});
 Route::get('/destination', function () {
     return view('destination');
 });
+
 Route::get('/culinary', function () {
     return view('culinary');
 });
+
 Route::get('/event', function () {
     return view('event');
 });
+
 Route::get('/package', function () {
     return view('tour-package');
 });
+
+// Owner Routes
+Route::get('/owner', function () {
+    return view('dashboardowner');
+});
+
+Route::get('/owner/manage-events', function () {
+    return view('manageevents');
+});
+
+Route::get('/owner/profile', function () {
+    return view('halkelolaprofil');
+});
+
+Route::get('/owner/submission', function () {
+    return view('submission');
+});
+
+Route::get('/owner/performance', function () {
+    return view('performance');
+});
+
